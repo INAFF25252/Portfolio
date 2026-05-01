69bbd5f31e6bfcc49c9dee2c4de0e48ed36fa7f8
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2026-05-01 10:10:01 -0400

    a

---
+++ portfolio/src/CAD.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>CAD Works</h1>
+  <p>
+  
+  </p>
+</template>
+
+<style scoped></style>
--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -1,9 +1,16 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import CAD from './CAD.vue';
 
-const app = createApp(App)
-
-app.use(createPinia())
-
-app.mount('#app')
+const routes = [
+ { path: '/', component: CAD },
+ { path: '/food', component:  },
+];
+const router = createRouter({
+ history: createWebHistory(),
+ routes,
+});
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
