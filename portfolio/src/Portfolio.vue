1efe2725003f65e34033227e692cc4a1b085668d
INAFF25252 <113192898+INAFF25252@users.noreply.github.com>
2026-05-29 11:46:16 -0400

    1

--- portfolio/src/Portfolio.vue
+++ portfolio/src/Portfolio.vue
@@ -4,10 +4,13 @@
     <section id="projects" class="flexbox flex justify-right content-evenly py-16">
     <div class="max-w-10xl mx-auto px-4">
       <h3 class="text-3xl font-bold text-[#221E22] mb-10 text-center">Art Projects</h3>
-      <div class="grid md:grid-cols-1 gap-8">
+      <div class="grid md:grid-cols-2 gap-8">
        
      
        
+ <div>
+   <h3 class="text-3xl font-bold text-[#221E22] mb-10 text-center">blah</h3>
+  </div>
 
  <div class="bg-white rounded-lg shadow  transition-transform transition-shadow">
           <img src="\public\images\IMG_2988.JPG" alt="Project 1" class="rounded-t-lg card-img-top img-fluid">
@@ -17,6 +20,10 @@
           </div>
         </div>
 
+  <div>
+   <h3 class="text-3xl font-bold text-[#221E22] mb-10 text-center">blah</h3>
+  </div>
+
  <div class="bg-white rounded-lg shadow  transition-transform transition-shadow">
           <img src="\public\images\IMG_2987.JPG" alt="Project 1" class="rounded-t-lg card-img-top img-fluid">
           <div class="p-5">
@@ -25,6 +32,11 @@
           </div>
         </div>
 
+         <div>
+   <h3 class="text-3xl font-bold text-[#221E22] mb-10 text-center">blah</h3>
+  </div>
+
+
          <div class="bg-white rounded-lg shadow  transition-transform transition-shadow">
           <img src="\public\images\IMG_2984.JPG" alt="Project 1" class="rounded-t-lg card-img-top img-fluid">
           <div class="p-5">
@@ -32,6 +44,11 @@
             <p class="text-orange-600 mb-4"></p>
           </div>
         </div>
+
+         <div>
+   <h3 class="text-3xl font-bold text-[#221E22] mb-10 text-center">blah</h3>
+  </div>
+
 
          <div class="bg-white rounded-lg shadow  transition-transform transition-shadow">
           <img src="\public\images\IMG_2981.JPG" alt="Project 1" class="rounded-t-lg card-img-top img-fluid">
